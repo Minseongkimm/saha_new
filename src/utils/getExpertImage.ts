3ef6d704903f9185b4018d872198b6401940a033
Minseongkimm <kms0206mm@gmail.com>
2025-10-20 11:16:29 +0900

    feat: 도사 이미지 시스템 완전 개편
    - 새로운 도사 이미지 10개 추가 (jangsaeng, healing, success, career, tongchal, cheongwang, sangpyeong, hoshi, yeonhwa, sangtong)
    - getExpertImage.ts 파일 정리 및 최적화
    
    이미지 시스템: 10명 도사 완전 커버 (연애, 직장, 사업, 건강, 운세 등 모든 영역)

--- src/utils/getExpertImage.ts
+++ src/utils/getExpertImage.ts
@@ -1,18 +1,16 @@
 export const getExpertImage = (imageName: string) => {
   const images: { [key: string]: any } = {
-    // 기존 도사님들
-    'hoosi_guy.jpg': require('../../assets/people/hoosi_guy.jpg'),
-    'yeonhwa_girl.jpg': require('../../assets/people/yeonhwa_girl.jpg'),
-    'cheongwang_guy.jpg': require('../../assets/people/cheongwang_guy.jpg'),
-    'sangtong_guy.jpg': require('../../assets/people/sangtong_guy.jpg'),
-    
-    // 새로운 6명의 도사님들
-    'yeonjeong_dosa.jpg': require('../../assets/people/yeonhwa_girl.jpg'), // 연정도사 - 연애 전문
-    'hyeonun_dosa.jpg': require('../../assets/people/sangtong_guy.jpg'), // 현운도사 - 재물/직업 전문
-    'myeongyeon_dosa.jpg': require('../../assets/people/cheongwang_guy.jpg'), // 명연도사 - 인생방향 전문
-    'dodam_dosa.jpg': require('../../assets/people/hoosi_guy.jpg'), // 도담도사 - 가족 전문
-    'unmyeong_dosa.jpg': require('../../assets/people/cheongwang_guy.jpg'), // 운명도사 - 시기/변화 전문
-    'cheongmyeong_dosa.jpg': require('../../assets/people/hoosi_guy.jpg'), // 청명도사 - 마음/불안 전문
+    // 현재 사용 중인 도사님들
+    'hoshi.jpg': require('../../assets/people/hoshi.jpg'),
+    'yeonhwa.jpg': require('../../assets/people/yeonhwa.jpg'),
+    'sangtong.jpg': require('../../assets/people/sangtong.jpg'),
+    'jangsaeng.jpg': require('../../assets/people/jangsaeng.jpg'),
+    'healing.jpg': require('../../assets/people/healing.jpg'),
+    'success.jpg': require('../../assets/people/success.jpg'),
+    'career.jpg': require('../../assets/people/career.jpg'),
+    'tongchal.jpg': require('../../assets/people/tongchal.jpg'),
+    'cheongwang.jpg': require('../../assets/people/cheongwang.jpg'),
+    'sangpyeong.jpg': require('../../assets/people/sangpyeong.jpg'),
   };
   return images[imageName];
 };
