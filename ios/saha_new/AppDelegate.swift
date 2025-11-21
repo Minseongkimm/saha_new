df59c0a1b67102709bafe86ca6b4dcbfd42b6994
Minseongkimm <kms0206mm@gmail.com>
2025-11-21 17:28:31 +0900

    refactor: AppDelegate moduleName 변경 (saha_new -> saha)

--- ios/saha_new/AppDelegate.swift
+++ ios/saha_new/AppDelegate.swift
@@ -35,7 +35,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "saha_new",
+      withModuleName: "saha",
       in: window,
       launchOptions: launchOptions
     )
