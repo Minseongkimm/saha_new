e8c19bd8b295477fe99c6590c1a1773729b98b9c
Minseongkimm <kms0206mm@gmail.com>
2025-12-03 15:33:11 +0900

    refactor: 앱 이름을 saha로 통일 및 Android 설정 수정
    - app.json, package.json: 앱 이름 saha_new → saha로 통일
    - MainActivity.kt: getMainComponentName() 반환값 saha로 변경
    - strings.xml: app_name을 saha로 변경
    - settings.gradle: rootProject.name을 saha로 변경

--- android/app/src/main/java/com/saha/MainActivity.kt
+++ android/app/src/main/java/com/saha/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "saha_new"
+  override fun getMainComponentName(): String = "saha"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
